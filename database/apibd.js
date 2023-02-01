83acf9aac19f2efeb800412d33aa420cd4adf509
Leo Serrano <leovpc@gmail.com>
2023-02-01 17:52:28 -0600

    a litle change

--- database/apibd.js
+++ database/apibd.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const apibd = express();
 const mysql = require("mysql2");
+const cors = require("cors");
 const credentials = require("./dbconnections")
+
+apibd.use(cors());
 
 apibd.get("/api/artistas", (req, res) => {
     const connection = mysql.createConnection(credentials);
@@ -14,13 +17,23 @@ apibd.get("/api/artistas", (req, res) => {
     });
   
   });
-  
-  
   apibd.get("/api/artistas/producciones", (req, res) => {
     const connection = mysql.createConnection(credentials);
-    connection.query('SELECT * FROM producciones', (error, result) =>{
+    connection.query('SELECT producciones.*, artistas.nombre_artista, artistas.instagram, artistas.imagen FROM producciones INNER JOIN artistas ON producciones.id_artista = artistas.id ORDER BY producciones.id DESC', (error, result) =>{
       if(error) {
         res.status(500).send(error);
+      }else {
+        res.status(200).send(result);
+      }
+    });
+  });
+  
+  apibd.get("/api/artistas/:artista", (req, res) => {
+    const artista = req.params.artista
+    const connection = mysql.createConnection(credentials);
+    connection.query(`SELECT artistas.nombre_artista, producciones.nombre FROM artistas INNER JOIN producciones ON artistas.id = producciones.id_artista WHERE artistas.nombre_artista = ${artista}`, (error, result) =>{
+      if(error) {
+        res.status(500).send(error); 
       }else {
         res.status(200).send(result);
       }
@@ -60,5 +73,6 @@ apibd.get("/api/artistas", (req, res) => {
       }
     });
   });
+ 
 
   module.exports = apibd
