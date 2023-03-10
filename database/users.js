c9c65844bcdc7db8abf24fdfde5e948f92b4cff6
Nikky <nikolgomez05@gmail.com>
2023-03-09 21:21:37 -0600

    listo ya esta funcionando el componente signin

--- database/users.js
+++ database/users.js
@@ -9,6 +9,9 @@ const MySQLStore = require('express-mysql-session')(session);
 const connection = mysql.createConnection(credentials);
 const connection2 = mysql2.createPool(credentials);
 const sessionStore = new MySQLStore({}/* session store options */, connection2);
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const sess = {
   key: 'sessionId',
   secret: "music oso",
@@ -36,48 +39,43 @@ app.use(express.json());
 app.post("/api/login", (req, res) => {
   const { email, password } = req.body;
 
-  // Creamos una consulta preparada con marcadores de posición
-  const query = "SELECT * FROM usuarios WHERE email = ? AND password = ?";
-  const values = [email, password];
+  
+// Creamos una consulta preparada con marcadores de posición
+const query = "SELECT * FROM usuarios WHERE email = ?";
+const values = [email];
 
-  // Ejecutamos la consulta preparada con los valores de los marcadores de posición
-  connection.query(query, values, (error, results) => {
-    if (error) {
-      // Enviamos una respuesta de error si hay un error en la consulta
-      res.status(500).json({ message: "Error en la consulta, el usuario no existe" });
-    } else if (results.length === 1) {
-      
-      req.session.isLoggedIn = true;
-      req.session.user_id = results[0].id;
-      req.session.role = results[0].role_id;
-      handlelogin();
-      
-    } else {
-      // Enviamos una respuesta de error si las credenciales son inválidas
-      res.status(401).json({ message: "Credenciales inválidas, verifica y vuelve a intentar" });
-    }
-    
+// Ejecutamos la consulta preparada con los valores de los marcadores de posición
+connection.query(query, values, (error, results) => {
+  if (error) {
+    // Enviamos una respuesta de error si hay un error en la consulta
+    res.status(500).json({ message: "Error en la consulta, el usuario no existe" });
+  } else if (results.length === 1) {
+    // Comparamos el password hasheado almacenado en la base de datos con el password proporcionado en la solicitud de login
+    bcrypt.compare(password, results[0].password, (error, isMatch) => {
+      if (isMatch) {
+        req.session.isLoggedIn = true;
+        req.session.user_id = results[0].id;
+        req.session.role = results[0].role_id;
+        handlelogin();
+      } else {
+        // Enviamos una respuesta de error si las credenciales son inválidas
+        res.status(401).json({ message: "Credenciales inválidas, verifica y vuelve a intentar" });
+      }
+    });
+  } else {
+    // Enviamos una respuesta de error si las credenciales son inválidas
+    res.status(401).json({ message: "Credenciales inválidas, verifica y vuelve a intentar" });
+  }
+
     function handlelogin() {
       if (process.env.NODE_ENV === 'production') {
         res.cookie("sessionId", req.session.id, { httpOnly: true, secure: true, maxAge: 3600000 }); // Establecemos la cookie de sesión
-        
+
       } else {
         res.cookie("sessionId", req.session.id, { httpOnly: true, secure: false, maxAge: 3600000 });
       }
       res.status(200).json({ message: "Inicio de sesión exitoso!", isLoggedIn: true, userId: req.session.user_id, role: req.session.role });
 
-      //actualiza los valores de la tabla de usuarios
-      /* const valor = req.session.id;
-      const id = results[0].id;
-      connection.query(
-        'UPDATE usuarios SET session_id = ? WHERE id = ?',
-        [valor, id],
-        (error, results) => {
-          if (error) {
-            console.error(error);
-          }
-        }
-      ); */
     }
   });
 });
@@ -85,17 +83,6 @@ app.post("/api/login", (req, res) => {
 
 app.post("/api/logout", (req, res) => {
 
-  // primero eliminamos el session_id de la tabla de usuarios, luego, Destruimos la sesión del usuario y eliminamos la cookie de sesión
- /* const sessId = req.session.id
-   connection.query(
-    'UPDATE usuarios SET session_id = ? WHERE session_id = ?',
-    [null, sessId],
-    (error, results) => {
-      if (error) {
-        console.error(error);
-      }
-    }
-  ); */
   req.session.isLoggedIn = false;
   res.status(200).json({ message: "Cierre de sesión exitoso!" });
 });
@@ -110,22 +97,55 @@ app.get("/api/check-session", (req, res) => {
 
 app.get('/api/usuarios/:id', (req, res) => {
   const id = parseInt(req.params.id);
-  if(id === req.session.user_id && req.session.isLoggedIn) {
+  if (id === req.session.user_id && req.session.isLoggedIn) {
     const query = `SELECT * FROM usuarios WHERE id = ?`;
     const values = [id];
-    connection.query(query, values,(error, results, fields) => {
+    connection.query(query, values, (error, results, fields) => {
       if (error) {
         console.error(error);
-      }else{
-        res.status(200).json({user_data: results[0]});
+      } else {
+        res.status(200).json({ user_data: results[0] });
 
       }
     });
-  }else{
+  } else {
     res.status(401).json({ message: "No tienes permiso para acceder a este recurso." });
   }
 
 });
 
 
+app.post("/api/signup", (req, res) => {
+  const { email, password, username,fecha_creacion, role_id } = req.body;
+
+  // Hashea el password utilizando bcrypt
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Ha ocurrido un error al crear el usuario" });
+      return;
+    }
+
+    // Crea un objeto con los datos del nuevo usuario y el password hasheado
+    const newUser = {
+      username: username,
+      password: hash,
+      email: email,
+      fecha_creacion: fecha_creacion,
+      role_id: role_id
+    };
+
+    // Inserta el nuevo usuario en la tabla de usuarios
+    connection.query('INSERT INTO usuarios SET ?', newUser, function (error, results, fields) {
+      if (error) {
+        console.error(error);
+        res.status(500).json({ error: "Ha ocurrido un error al crear el usuario" });
+        return;
+      }
+
+      res.status(200).json({ message: "Nuevo usuario creado con éxito" });
+      console.log('Nuevo usuario creado con éxito');
+    });
+  });
+});
 module.exports = app;
