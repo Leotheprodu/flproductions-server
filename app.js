298da2b8719e1d0765dd9bf89fbd06a7a7cacd9c
LeotheProdu <leovpc@gmail.com>
2023-04-13 03:38:58 -0600

    he cambiaro el origin de cors a el puerto 3000

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const sess = {
 }
 
 app.use(cors({
-  origin: "http://localhost:5173", // use your actual domain name (or localhost), using * is not recommended
+  origin: "http://localhost:3000", // use your actual domain name (or localhost), using * is not recommended
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
   credentials: true
