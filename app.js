58c109e1bb0b12d408e195919e14df9bc7aa0fca
LeotheProdu <leovpc@gmail.com>
2023-04-22 01:15:49 -0600

    agregue la funcionalidad de crear log, local o enviarlos por slack

--- app.js
+++ app.js
@@ -2,11 +2,27 @@
 require('dotenv').config({ override: true })
 const express = require("express");
 const cors = require("cors");
+const morganBody = require("morgan-body");
+const {loggerstream, log} = require('./config/logger');
 const { dbConnectMySql } = require('./config/mysql');
 const PUERTO = process.env.PORT || 5000;
 const session = require('express-session');
 const sess = require('./config/expressSessions');
 const app = express();
+
+morganBody(app,{
+  noColors: true,
+  stream: log,
+  logRequestBody: false,
+
+});
+morganBody(app,{
+  noColors: true,
+  stream: loggerstream,
+  skip: function(req, res){
+    return res.statusCode < 400;
+  }
+});
 
 
 app.use(cors({
@@ -21,15 +37,10 @@ app.use(express.static('storage'));
 app.use(session(sess));
 app.use('/api', require('./routes'));
 
+
 app.use((err, req, res, next) => {
   res.status(500).send('Ocurrió un error en el servidor');
 });
-
-
-/* if (app.get('env') === 'production') {
-  app.set('trust proxy', 1) // trust first proxy
-  sess.cookie.secure = true // serve secure cookies
-} */
 
 const server = app.listen(PUERTO, () => {
 console.log(`El servidor esta escuchando en el puerto ${PUERTO}...`);
