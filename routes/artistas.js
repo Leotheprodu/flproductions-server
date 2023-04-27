9a19409f7c3544d032b6786a3206e774b408741d
LeotheProdu <leovpc@gmail.com>
2023-04-27 01:20:21 -0600

    he quitado el rateLimit de artistas getitems

--- routes/artistas.js
+++ routes/artistas.js
@@ -6,9 +6,9 @@ const { checkRoles } = require("../middleware/roles");
 const rateLimiter = require("../config/rate-limit");
 
 /* Lista los items */
-router.get("/",rateLimiter, getItems);
+router.get("/", getItems);
 /* Obtener Item */
-router.get("/:id", rateLimiter, validatorGetItem,checkRoles([3,4,5]), getItem);
+router.get("/:id", rateLimiter, validatorGetItem, getItem);
 /* Crea un registro */
 router.post("/", rateLimiter, validatorCreateItem, checkRoles([3,4,5]), createItem);
 /* Actualiza un Registro */
